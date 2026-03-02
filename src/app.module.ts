d0e5a7d63495346c18cf136fd236f5f4de2f026e
Sabrina Novaes <sabrinanovaes96@gmail.com>
2026-03-02 10:27:12 -0300

    🎉 Criação do Blog Pessoal

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [ // conexão com o banco de dados
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_blogpessoal',
+      entities: [],
+      synchronize: true, // Sincroniza o banco de dados com as entidades definidas no código, criando ou atualizando as tabelas conforme necessário. Isso é útil durante o desenvolvimento para garantir que o esquema do banco de dados esteja sempre atualizado com as mudanças nas entidades, mas deve ser usado com cuidado em ambientes de produção para evitar perda de dados acidental.
+    }) 
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
